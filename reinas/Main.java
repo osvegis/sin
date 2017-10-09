b9e897a80348e2e3984397339d7ec0d75a032f52
Oscar Vega Gisbert <oscar@tetrainfo.com>
2017-10-09 16:06:12 +0200

    8 reinas: mejoras para n reinas (recursivo e iterativo)

--- reinas/Main.java
+++ reinas/Main.java
@@ -4,48 +4,115 @@
  */
 package reinas;
 
-import java.util.*;
-
 /**
- * Problema de las 8 reinas.
+ * Problema de las N reinas.
  */
 public class Main
 {
-private static int nodosExplorados;
+// Columnas ocupadas por las reinas en cada fila:
+// reinas[y] indica en qué columna está la reina de la fila y.
+private static int[] reinas;
 
-private static void busquedaProfundidad(Estado e, ArrayList<Estado> sol)
+private static boolean
+    rx[], // Columnas ocupadas por reinas.
+    dr[], // Diagonales de izquierda a derecha ocupadas.
+    ds[]; // Diagonales de derecha a izquierda ocupadas.
+
+private static int soluciones; // Número de soluciones encontradas.
+
+private static void busquedaRecursiva(int y)
 {
-    nodosExplorados++;
+    int r, s, n = reinas.length;
 
-    if(e.esObjetivo())
+    if(y == n)
     {
-        sol.add(e);
+        printSolucion();
+        return;
     }
-    else if(e.getNumReinas() < 8)
-    {
-        for(int i = 0; i < 8; i++)
-        {
-            Estado s = e.sucesor(i);
 
-            if(s != null)
-                busquedaProfundidad(s, sol);
+    for(int x = 0; x < n; x++)
+    {
+        if(!rx[x] && !dr[r = n + x - y] && !ds[s = x + y])
+        {
+            rx[x] = dr[r] = ds[s] = true;
+            reinas[y] = x;
+            busquedaRecursiva(y + 1);
+            rx[x] = dr[r] = ds[s] = false;
         }
     }
 }
 
-public static void main(String args[])
+private static void busquedaIterativa()
 {
-    nodosExplorados = 0;
-    ArrayList<Estado> soluciones = new ArrayList<>();
-    busquedaProfundidad(new Estado(), soluciones);
+    int r, s, x = 0, y = 0, n = reinas.length;
 
-    for(int i = 0; i < soluciones.size(); i++)
+    for(;;)
     {
-        System.out.println("Solución: "+ (i+1) +"\n");
-        System.out.println(soluciones.get(i));
+        if(y > 0)
+        {
+            if(y == n)
+                printSolucion();
+
+            y--;
+            x = reinas[y];
+            rx[x] = dr[n + x - y] = ds[x + y] = false;
+            x++;
+
+            if(y == 0 && x == n)
+                break;
+        }
+
+        while(x < n)
+        {
+            if(!rx[x] && !dr[r = n + x - y] && !ds[s = x + y])
+            {
+                rx[x] = dr[r] = ds[s] = true;
+                reinas[y] = x;
+                y++;
+                x = 0;
+            }
+            else
+            {
+                x++;
+            }
+        }
+    }
+}
+
+private static void printSolucion()
+{
+    StringBuilder sb = new StringBuilder();
+
+    for(int y = 0; y < reinas.length; y++)
+    {
+        int r = reinas[y];
+
+        for(int x = 0; x < reinas.length; x++)
+            sb.append(r == x ? " X" : " ·");
+
+        sb.append('\n');
     }
 
-    System.out.println(nodosExplorados +" nodos explorados.");
+    soluciones++;
+    System.out.println("Solución: "+ soluciones +"\n");
+    System.out.println(sb);
+}
+
+public static void main(String args[])
+{
+    int n = 8;
+
+    if(args.length == 1)
+        n = Integer.parseInt(args[0]);
+
+    System.out.println(n +" reinas");
+    reinas = new int[n];
+    rx = new boolean[n];
+    dr = new boolean[2 * n];
+    ds = new boolean[2 * n];
+
+    //busquedaRecursiva(0);
+    busquedaIterativa();
 }
 
 } // Main
